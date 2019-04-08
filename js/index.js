618c71e5fe94e334133aae0ec6a4689c4c261ccf
davichodavid <davichodavid@yahoo.com>
2019-04-08 17:01:25 -0400

    Finished the nav, working on the top-content

--- js/index.js
+++ js/index.js
@@ -14,29 +14,54 @@ const siteContent = {
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
+    "features-h4": "Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
+    "about-h4": "About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
+    "services-h4": "Services",
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
+    "product-h4": "Product",
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
+    "vision-h4": "Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
   "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+    "contact-h4": "Contact",
+    "address": "123 Way 456 Street Somewhere, USA",
+    "phone": "1 (888) 888-8888",
+    "email": "sales@greatidea.io",
   },
   "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+    "copyright": "Copyright Great Idea! 2018"
   },
 };
 
-// Example: Update the img src for the logo
+
+//************* Navigation *****************
+const navAs = Array.from(document.querySelectorAll(" header a"));
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+navAs.map(function (anchor, index) {
+  return anchor.textContent = Object.values(siteContent.nav)[index];
+});
+
+logo.src = siteContent.nav["img-src"];
+
+//************* CTA *****************
+const ctaH1 = document.querySelector(".cta h1").textContent = siteContent.cta["h1"];
+const ctaButton = document.querySelector(".cta button").textContent = siteContent.cta["button"];
+const ctaImg = document.querySelector("#cta-img").src = siteContent.cta["img-src"];
+
+//************* Main Content *****************
+const featuresH4 = document.querySelectorAll(".top-content .text-content h4")[0].textContent = siteContent["main-content"]["features-h4"];
+const featuresContent = document.querySelectorAll("top-content .text-content p")[0].textContent = siteContent["main-content"]["features-content"];
+const aboutH4 = document.querySelectorAll("top-content .text-content h4")[1].textContent = siteContent["main-content"]["about-h4"];
+const aboutContent = document.querySelector("top-content .text-content p")[1].textContent = siteContent["main-content"]["about-content"];
+const middleImgSrc = document.querySelector("#middle-img").src = siteContent["main-content"]["middle-img-src"];
+const servicesH4 = document.querySelectorAll(".bottom-content .text-content h4")[0].textContent = siteContent["main-content"]["services-h4"];
+const servicesContent = document.querySelector(".bottom-content .text-content p")[0].textContent = siteContent["main-content"]["services-content"];
+const productH4 = document.querySelector(".bottom-content .text-content h4")[1].textContent = siteContent["main-content"]["product-h4"];
+const productContent = document.querySelector(".bottom-content .text-content p")[1].textContent = siteContent["main-content"]["product-content"];
+const visionH4 = document.querySelector(".bottom-content .text-content h4")[2].textContent = siteContent["main-content"]["vision-h4"];
+const visionContent = document.querySelector(".bottom-content .text-content p")[2].textContent = siteContent["main-content"]["vision-content"];
